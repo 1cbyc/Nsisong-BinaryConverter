05f542bcc74fddf289f9341242c58d8b9b8f27ce
Isaac Nsisong <35623334+isaacnsisong@users.noreply.github.com>
2021-07-25 13:53:45 +0100

    Create converter.cs

---
+++ converter.cs
@@ -0,0 +1,44 @@
+/* By Isaac Nsisong */
+/* Instagram: @NsisongLabs */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* A binary number is made up of elements called bits where each bit can be 1 or 0 */
+namespace SoloLearn
+{
+    class Program
+    {
+        static string toBinary(int n)
+        {
+            if (n == 0)
+            {
+                return "0";
+            }
+    
+            string binary = "";
+            while (n > 0)
+            {
+                // The % (modulus) operator returns the remainder of the division
+                int rem = n % 2;
+                // add the result (0 or 1)
+                binary = rem + binary;
+                // divide the number by 2
+                n = n / 2;
+            }
+    
+            return binary;
+        }
+        
+        static void Main(string[] args)
+        { 
+            //sample number
+            int number = 156;
+            
+            Console.WriteLine(toBinary(number));
+        }
+    }
+}
